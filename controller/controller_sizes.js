8bcfdb473e7366e79a109df05f976729a83aa4ac
Dina <dina.g@cym.cio>
2021-12-27 20:53:20 +0200

    example of addSizes API

--- controller/controller_sizes.js
+++ controller/controller_sizes.js
@@ -4,6 +4,8 @@ const service_sizes =require ("../service/service_sizes");
 async function addSizes(req, res) {
     try {
         const mySizes = req.body;
+        // here we will check if user valid by token (next step)
+        // we will add user identifiers to the addSize function to save sizes for specific client
         await service_sizes.addSizes(mySizes);
     }
     catch (error) { console.log(error);
